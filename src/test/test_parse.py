eb87cc3dbf548a1c79df7efe036428a5ae593d44
Benoit Hamelin <benoit@benoithamelin.com>
2026-02-23 13:02:31 -0500

    Improve script metadata parsing robustness

--- src/test/test_parse.py
+++ src/test/test_parse.py
@@ -1,7 +1,16 @@
 import pytest  # noqa
 from textwrap import dedent
+from typing import Type
+from warnings import catch_warnings
 
-from uvk._parse import parse_dependencies, parse_script_metadata
+from uvk._parse import (
+    IllegalLine,
+    NoScriptMetadataEndLine,
+    NoScriptMetadataStartLine,
+    parse_dependencies,
+    parse_script_metadata,
+    TrailingLines,
+)
 
 
 @pytest.mark.parametrize(
@@ -34,6 +43,14 @@ from uvk._parse import parse_dependencies, parse_script_metadata
 )
 def test_parse_dependencies(line: str, expected: list[str]) -> None:
     assert expected == parse_dependencies(line)
+
+
+def cook_metadata(metadata_raw: str) -> str:
+    """
+    Transforms metadata string made to fit Python indent structure into unindented
+    paragraphs as expected in the wild.
+    """
+    return dedent(metadata_raw.rstrip())
 
 
 @pytest.mark.parametrize(
@@ -126,7 +143,120 @@ def test_parse_dependencies(line: str, expected: list[str]) -> None:
                 },
             },
         ),
+        (
+            """\
+            # Here is the script metadata
+            #
+            # /// script
+            # requires-python = ">3.10"
+            # ///
+            """,
+            {"requires-python": ">3.10"},
+        ),
     ],
 )
 def test_parse_script_metadata_correct(metadata_raw: str, expected: dict) -> None:
-    assert expected == parse_script_metadata(dedent(metadata_raw.rstrip()))
+    assert expected == parse_script_metadata(cook_metadata(metadata_raw))
+
+
+@pytest.mark.parametrize(
+    "metadata_raw,expected",
+    [
+        (
+            "",
+            NoScriptMetadataStartLine,
+        ),
+        (
+            """\
+            # dependencies = [
+            #     "numpy",
+            #     "requests",
+            # ]
+            """,
+            NoScriptMetadataStartLine,
+        ),
+        (
+            """\
+            #/// script
+            # requires-python = ">3.10"
+            # ///
+            """,
+            IllegalLine,
+        ),
+        (
+            """\
+            # ///script
+            # requires-python = ">3.10"
+            # ///
+            """,
+            NoScriptMetadataStartLine,
+        ),
+        (
+            """\
+            # ///  script
+            # requires-python = ">3.10"
+            # ///
+            """,
+            NoScriptMetadataStartLine,
+        ),
+        (
+            """\
+            # /// script
+            # requires-python = ">3.10"
+            """,
+            NoScriptMetadataEndLine,
+        ),
+        (
+            """\
+            # /// script
+            # requires-python = ">3.10"
+            #///
+            """,
+            IllegalLine,
+        ),
+        (
+            """\
+            # /// script
+            # requires-python = ">3.10"
+            # /// end
+            """,
+            NoScriptMetadataEndLine,
+        ),
+        (
+            """\
+            # /// script
+            requires-python = ">3.10"
+            # /// end
+            """,
+            IllegalLine,
+        ),
+        (
+            """\
+            /// script
+            # requires-python = ">3.10"
+            # ///
+            """,
+            IllegalLine,
+        ),
+    ],
+)
+def test_parse_script_metadata_break_convention(
+    metadata_raw: str, expected: Type[ValueError]
+) -> None:
+    with pytest.raises(expected):
+        parse_script_metadata(cook_metadata(metadata_raw))
+
+
+def test_parse_script_metadata_warn_trailing_lines():
+    with catch_warnings(record=True, category=TrailingLines) as ws:
+        parse_script_metadata(
+            cook_metadata(
+                """\
+                # /// script
+                # requires-python = ">3.10"
+                # ///
+                # asdf
+                """,
+            )
+        )
+    assert len(ws) == 1
--- src/uvk/_parse.py
+++ src/uvk/_parse.py
@@ -2,6 +2,7 @@ from collections.abc import Iterator, Sequence
 import logging as lg
 import re
 import tomllib
+import warnings as w
 
 Requirements = Sequence[str]
 LOG = lg.getLogger(__name__.split(".")[0])
@@ -9,13 +10,6 @@ LOG = lg.getLogger(__name__.split(".")[0])
 
 def parse_dependencies(deps: str) -> Requirements:
     return [dep.strip() for dep in deps.split()]
-
-
-def iter_lines_metadata(metadata: str) -> Iterator[str]:
-    for line in metadata.split("\n"):
-        line = line.strip()
-        if line.startswith("# "):
-            yield line
 
 
 class ScriptMetadataParseError(ValueError):
@@ -43,28 +37,57 @@ class NoScriptMetadataEndLine(ScriptMetadataParseError):
         )
 
 
+class IllegalLine(ScriptMetadataParseError):
+
+    def __init__(self, metadata: str) -> None:
+        super().__init__(
+            (
+                "An illegal line (neither `#` nor starting with `# `) appears among "
+                "the script metadata"
+            ),
+            metadata,
+        )
+
+
+def iter_lines_metadata(metadata: str) -> Iterator[str]:
+    for line in metadata.split("\n"):
+        line = line.strip()
+        if line in {"", "#"}:
+            pass
+        elif re.match(r"# ", line):
+            yield line[2:]
+        else:
+            raise IllegalLine(metadata)
+
+
+class TrailingLines(Warning):
+    pass
+
+
 def parse_script_metadata(metadata: str) -> dict:
     lines_metadata = iter_lines_metadata(metadata)
     try:
-        while not re.match(r"# /// script\w*$", next(lines_metadata)):
+        while not re.match(r"/// script\w*$", next(lines_metadata)):
             pass
     except StopIteration:
         raise NoScriptMetadataStartLine(metadata)
 
     lines_toml = []
-    try:
-        for line in lines_metadata:
-            if re.match(r"# ///\w*$", line):
-                break
-            lines_toml.append(line[2:])
-    except StopIteration:
+    for line in lines_metadata:
+        if re.match(r"///\w*$", line):
+            break
+        lines_toml.append(line)
+    else:
         raise NoScriptMetadataEndLine(metadata)
 
     try:
         next(lines_metadata)
-        LOG.warn(
-            "The script metadata has trailing lines after the closing line `# ///`; "
-            "these are ignored"
+        w.warn(
+            message=(
+                "The script metadata has trailing lines after the closing line "
+                "`# ///`; these are ignored"
+            ),
+            category=TrailingLines,
         )
     except StopIteration:
         pass
